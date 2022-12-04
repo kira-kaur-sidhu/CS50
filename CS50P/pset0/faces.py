1760bc4a264f67e13b1a871c8c4f02b4d88fced1
Kira Kaur Sidhu <111136730+kira-kaur-sidhu@users.noreply.github.com>
2022-12-04 10:00:28 -0500

    Create faces.py

---
+++ CS50P/pset0/faces.py
@@ -0,0 +1,11 @@
+# Faces
+
+def main():
+
+    # Input phrase and replace emoticons with emojis
+    faces = input().replace(":)", "🙂").replace(":(","🙁")
+
+    # Print phrase
+    print(faces)
+
+main()
