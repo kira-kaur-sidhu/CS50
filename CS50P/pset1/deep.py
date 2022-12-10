8b764c3c8dafb2e7f56adb57428ba73c00bc23b6
Kira Kaur Sidhu <111136730+kira-kaur-sidhu@users.noreply.github.com>
2022-12-09 22:43:05 -0500

    Update deep.py

--- CS50P/pset1/deep.py
+++ CS50P/pset1/deep.py
@@ -1,4 +1,4 @@
-# Deep
+# Deep - implement a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No.
 
 def main():
     x = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
