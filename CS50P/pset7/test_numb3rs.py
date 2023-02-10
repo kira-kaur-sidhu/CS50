a4da38cf344167d8660225717e5e4c62b3c7fcb2
Kira Kaur Sidhu <111136730+kira-kaur-sidhu@users.noreply.github.com>
2023-02-10 17:57:28 -0500

    Create test_numb3rs.py

---
+++ CS50P/pset7/test_numb3rs.py
@@ -0,0 +1,9 @@
+from numb3rs import validate
+
+def test_numbers():
+    assert validate("127.0.0.1") == True
+    assert validate("255.255.255.255") == True
+    assert validate("512.512.512.512") == False
+    assert validate("1.2.3.1000") == False
+    assert validate("cat") == False
+    assert validate("75.356.76.65") == False
