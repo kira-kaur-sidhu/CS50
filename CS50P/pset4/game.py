61aab2d6a23be897c6c95a9175a2cd69c12eb5bd
Kira Kaur Sidhu <111136730+kira-kaur-sidhu@users.noreply.github.com>
2023-02-10 17:38:15 -0500

    Create game.py

---
+++ CS50P/pset4/game.py
@@ -0,0 +1,32 @@
+# Guessing Game - implement a program that
+# Prompts the user for a level, if the user does not input a positive integer, the program should prompt again.
+# Randomly generates an integer between 1 and , inclusive, using the random module.
+# Prompts the user to guess that integer. If the guess is not a positive integer, the program should prompt the user again.
+# If the guess is smaller than that integer, the program should output Too small! and prompt the user again.
+# If the guess is larger than that integer, the program should output Too large! and prompt the user again.
+# If the guess is the same as that integer, the program should output Just right! and exit.
+
+import random
+
+while True:
+    try:
+        level = int(input("Level: "))
+        if level > 0:
+            number = random.randint(1, level)
+            break
+    except ValueError:
+        pass
+
+while True:
+    try:
+        guess = int(input("Guess: "))
+        if guess > 0:
+            if guess < number:
+                print("Too small!")
+            elif guess > number:
+                print("Too large!")
+            else:
+                print("Just right!")
+                break
+    except ValueError:
+        pass
