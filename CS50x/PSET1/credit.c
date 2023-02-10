d97a08b3a1e1f726c64d8c1e6c77e7483fc2a5e1
Kira Kaur Sidhu <111136730+kira-kaur-sidhu@users.noreply.github.com>
2023-02-10 18:35:30 -0500

    Update credit.c

--- CS50x/PSET1/credit.c
+++ CS50x/PSET1/credit.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int count(long n);
 int checksum(long n);
 
 int main(void)
@@ -8,18 +9,11 @@ int main(void)
     long n = get_long("Number: ");
 
     int check = checksum(n) % 10;
-    long n_sum = n;
-    int count = 0;
-    do{
-        n_sum = n_sum / 10;
-        ++count;
-    }
-    while (n_sum != 0);
+    int counts = count(n);
 
     if (check == 0)
     {
         long reverse_n = 0;
-
         while (n > 0)
         {
             reverse_n = reverse_n * 10 + n % 10;
@@ -27,15 +21,15 @@ int main(void)
         }
 
         int x = (reverse_n / 10) % 10;
-        if ((reverse_n % 10) == 3 && count == 15 && (x == 3 || x == 7))
+        if ((reverse_n % 10) == 3 && counts == 15 && (x == 3 || x == 7))
         {
             printf("%s\n", "AMEX");
         }
-        else if ((reverse_n % 10) == 4 && (count == 13 || count == 16))
+        else if ((reverse_n % 10) == 4 && (counts == 13 || counts == 16))
         {
             printf("%s\n", "VISA");
         }
-        else if ((reverse_n % 10) == 5 && count == 16 && (x == 1 || x == 2 || x == 3 || x == 4 || x == 5))
+        else if ((reverse_n % 10) == 5 && counts == 16 && (x == 1 || x == 2 || x == 3 || x == 4 || x == 5))
         {
             printf("%s\n", "MASTERCARD");
         }
@@ -51,23 +45,25 @@ int main(void)
     }
 }
 
+int count(long n)
+{
+    int count = 0;
+    do{
+        n = n / 10;
+        ++count;
+    }
+    while (n != 0);
+    return count;
+}
 
 int checksum(long n)
 {
-    long n_sum = n;
     int sum_even = 0;
     int sum_odd = 0;
-    int count = 0;
+    int counts = count(n);
 
-    do{
-        n_sum = n_sum / 10;
-        ++count;
-    }
-    while (n_sum != 0);
-
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i < counts; i++)
     {
-
         long number = n % 10;
 
         if (i%2 == 0)
@@ -76,14 +72,9 @@ int checksum(long n)
         }
         else
         {
-            int even1 = (number * 2) % 10;
-            int even2 = (number * 2) / 10;
-
-            sum_even += (even1) + (even2);
+            sum_even += ((number * 2) % 10) + ((number * 2) / 10);
         }
-        n -= number;
-        n = n / 10;
+        n = (n -number) / 10;
     }
-    int total = sum_even + sum_odd;
-    return total;
+    return (sum_even + sum_odd);
 }
